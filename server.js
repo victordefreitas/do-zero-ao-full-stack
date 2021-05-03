f1a321a5a62e616100a026eb55d314f6e93a9bf5
lucas <comercial@wavii.com.br>
2021-05-03 19:18:03 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const api = require('./app/Routes')
+
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.json({
+        'success': true
+    });
+})
+
+app.use('/api', api);
+
+const PORT = process.env.PORT;
+//var port = 8080;
+console.log(PORT)
+app.listen(8080)
+
+
+//user : My-portfolio
+//senha: 180279
+//Url: mongodb+srv://My-portfolio:<password>@cluster0.5xwzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
